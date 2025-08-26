9b5030256cdf27f3808695357c42833dfdcc0c7d
yogeshkp <yogeshpatel5117@gmail.com>
2025-08-27 02:10:11 +0530

    createbasicreactform

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Form from './components/Form'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+<Form/>
+
+      </>
+  )
+}
+
+export default App
